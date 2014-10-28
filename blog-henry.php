80d9bcdfbb2ccb9731eac5ae17c067deacef190f
Henry Ramirez <h.ramirez0427@gmail.com>
2014-10-28 09:11:01 -0700

    commit 1
    php project

---
+++ blog-henry.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+<title></title>
+</head>
+<body>
+	<?php 
+    require_once(__DIR__ . "/controller/create-db.php");
+	 ?>
+</body>	
+</html>
---
+++ controller/create-db.php
@@ -0,0 +1,23 @@
+<?php
+
+require_once(__DIR__ . "/../database/database.php");
+/*directory of the file. If used inside an include, the 
+directory of the included file is returned. */
+$connection = new mysqli($host,$username,$password);
+
+if($connection->connect_error) {
+	die("Error: " . $connection->connection_error);
+}
+
+$exists = $connection->select_db($database);
+/* this is used to see if the database exists*/
+if(!$exists) {
+	$query = $connection->query("CREATE DATABASE $database");
+    /*this is used to create a database.*/
+
+    if ($query) {
+    	echo "Successfully created database;" . $database;
+    }
+}
+
+$connection->close();
