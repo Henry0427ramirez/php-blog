499decdbf3dce7125e5a7afb83a55f111434ac85
Henry Ramirez <h.ramirez0427@gmail.com>
2014-11-03 10:40:38 -0800

    commit 9
    editing my code

--- blog-henry.php
+++ blog-henry.php
@@ -1,10 +1,6 @@
-<html>
-<head>
-<title></title>
-</head>
-<body>
-	<?php 
-    require_once(__DIR__ . "/controller/create-db.php");
-	 ?>
-</body>	
-</html>
+<?php 
+require_once(__DIR__ . "/header.php");
+require_once(__DIR__ . "/controller/create-db.php");
+require_once(__DIR__ . "/footer.php");
+?>
+  
