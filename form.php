45568e901b5edc94ade15fb7df0220e51ff526bc
Henry Ramirez <h.ramirez0427@gmail.com>
2014-11-05 09:03:45 -0800

    commit 12
    made a css

--- form.php
+++ form.php
@@ -4,10 +4,12 @@
    <div>
       <label for="title">Title: </label>
       <input type"text" name="title" />
+      <!-- the title takes up one line  -->
    </div>
 
    	<div>
    	  <label for="post">Post: </label>
    	  <textarea name="post"></textarea>
+   	  <!-- -->
     </div>
 </form>
