ed2118dc335e7bbc35abb213a3d0a828ea1a5e87
Henry Ramirez <h.ramirez0427@gmail.com>
2014-11-19 08:51:52 -0800

    commit 24
    refactoring code

--- controller/create-post.php
+++ controller/create-post.php
@@ -1,8 +1,6 @@
 <?php
 
    require_once(__DIR__ . "/../model/config.php");
-
-   $connection = new mysqli($host, $username, $password, $database);
    //your storing th connection from create-db into the create-post
 
    $title = filter_input(INPUT_POST,"title",FILTER_SANITIZE_STRING);
--- model/config.php
+++ model/config.php
@@ -1,7 +1,12 @@
 <?php
   $path = "/My-php-blog/";
   // this our new path to fixe some of our files.
- $host = "localhost";
- $username = "root";
- $password = "root";
- $database = "blog_db";
+  $host = "localhost";
+  $username = "root";
+  $password = "root";
+  $database = "blog_db";
+
+  $connection = new Database($host, $username, $password, $database);
+  //$host(the server) $username(name of host) password(server password and our database) 
+
+
