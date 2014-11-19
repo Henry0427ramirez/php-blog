220451480c92812276057d82857cfbf6a12af205
Henry Ramirez <h.ramirez0427@gmail.com>
2014-11-18 20:37:05 -0800

    commit 22
    using this file for new functions

--- model/database.php
+++ model/database.php
@@ -1,1 +1,19 @@
 <?php
+class Database {
+	private $connection;
+	private $host;
+	private $username;
+	private $password;
+	private $database;
+	//these variables will stay with us
+	//these varibles will be hidden when people access the file
+	//we use private to only access it here in this file
+
+	public function __construct ($host, $username, $password, $database) {
+		//these variables will disappear when the function is done.
+       $this->host = $host;
+       $this->username = $username;
+       $this->password = $password;
+       $this->database = $database;
+	}
+}
