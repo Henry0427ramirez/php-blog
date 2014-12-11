bbf280e56bbbad9f017280a4cf785717cfcd8aca
Henry Ramirez <h.ramirez0427@gmail.com>
2014-12-11 08:33:47 -0800

    commit 36
    adding my posts to my files

--- blog-henry.php
+++ blog-henry.php
@@ -3,5 +3,6 @@ require_once(__DIR__ . "/view/header.php");
 require_once(__DIR__ . "/view/navigation.php");
 require_once(__DIR__ . "/controller/create-db.php");
 require_once(__DIR__ . "/view/footer.php");
+require_once(__DIR__ . "/controller/read-posts.php");
 ?>
 <!-- we seperated our html into headers and footers so when we code we can use the headers and footers for multiple files instead of writing the code out all the time.-->
